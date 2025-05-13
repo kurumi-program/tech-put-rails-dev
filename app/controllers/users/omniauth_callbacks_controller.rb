f4dcdd05d136700f9c157323b051275aff645c43
kurumi-program <kurumi.program@gmail.com>
2025-05-13 23:17:59 +0900

    gooleログイン設定追加

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,11 +3,11 @@ class Users::OmniauthCallbacksController < ApplicationController
     @user = User.from_omniauth(request.env['omniauth.auth'])
 
     if @user.persisted?
-      # トークンを生成してユーザーをサインイン
+      
       @user.create_new_auth_token
       render json: @user, status: :ok
     else
-      # ユーザー作成に失敗した場合
+     
       redirect_to new_user_registration_url
     end
   end
